37c6f417e7c0b5a12856452c27deec0083200339
rnikiforova <rnikiforova@gmail.com>
2013-08-14 16:10:17 +0300

    Updated MessagesController

--- WebChatAPI/Controllers/MessagesController.cs
+++ WebChatAPI/Controllers/MessagesController.cs
@@ -4,21 +4,45 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebChat.Models;
+using WebChat.Repository;
 
 namespace WebChatAPI.Controllers
 {
     public class MessagesController : ApiController
     {
-        // GET api/message
-        public IEnumerable<string> Get()
+        private IRepository<Message> data;
+
+        public MessagesController()
         {
-            return new string[] { "value1", "value2" };
+            this.data = new MessagesRepository();
         }
 
-        // GET api/message/5
-        public string Get(int id)
+        public void Send(int senderId, int receiverId, [FromBody]string content)
         {
-            return "value";
+            // Validate content
+
+            Message newMessage = new Message()
+            {
+                SenderId = senderId,
+                ReceiverId = receiverId,
+                Content = content
+            };
+
+            data.Add(newMessage);
+        }
+        
+        public ICollection<Message> GetInbox(int receiverId)
+        {
+            var allMesages = this.data.All();
+            var inbox = allMesages.Where(m => m.ReceiverId == receiverId).ToList();
+            return inbox;
+        }
+
+        public ICollection<Message> GetSent(int senderId)
+        {
+            var sentMessages = this.data.All().Where(m => m.SenderId == senderId).ToList();
+            return sentMessages;
         }
 
         // POST api/message
