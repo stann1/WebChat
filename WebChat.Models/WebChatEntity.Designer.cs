80cadf4983e5a325b61dd586e759eccae831a165
nikolay.marinov <nikolay.marinov76@gmail.com>
2013-08-15 11:12:17 +0300

    Delete solution

--- WebChat.Models/WebChatEntity.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\My files\_GITHUB\WebChat\WebChat.Models\WebChatEntity.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
